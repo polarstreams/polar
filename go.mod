bb31f469706335221498c8452c38b1e1aa89320f
Jorge Bay Gondra <jorgebaygondra@gmail.com>
2021-02-21 22:21:22 +0100

    Add http2 support

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mattn/go-sqlite3 v1.14.6 // indirect
 	github.com/rs/zerolog v1.20.0
+	golang.org/x/net v0.0.0-20210220033124-5f55cee0dc0d
 )
--- internal/producing/server.go
+++ internal/producing/server.go
@@ -7,6 +7,8 @@ import (
 	"github.com/jorgebay/soda/internal/configuration"
 	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog/log"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 type Producer interface {
@@ -30,16 +32,24 @@ func (p *producer) Init() error {
 }
 
 func (p *producer) AcceptConnections() error {
+	port := p.config.ProducerPort()
+	address := fmt.Sprintf(":%d", port)
 	router := httprouter.New()
     router.GET("/", Index)
 
+	h2s := &http2.Server{}
+	server := &http.Server{
+		Addr: address,
+		Handler: h2c.NewHandler(router, h2s),
+	}
+
+	http2.ConfigureServer(server, h2s)
+
 	c := make(chan bool, 1)
 	go func () {
-		port := p.config.ProducerPort()
-		address := fmt.Sprintf(":%d", port)
 		log.Info().Int32("port", port).Msg("Start listening to producers")
 		c <- true
-		if err := http.ListenAndServe(address, router); err != nil {
+		if err := server.ListenAndServe(); err != nil {
 			log.Fatal().Err(err)
 		}
 	}()
@@ -49,5 +59,5 @@ func (p *producer) AcceptConnections() error {
 }
 
 func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    fmt.Fprint(w, "Welcome!\n")
+    fmt.Fprint(w, "Hello from soda producer\n")
 }
