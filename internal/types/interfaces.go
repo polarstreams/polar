92fa3c273c85d21553d06ee5a43ba43bd7bc4247
Jorge Bay <jorgebaygondra@gmail.com>
2022-10-05 15:28:22 +0200

    Add CRC to interbroker message header
    Resolves #41.

--- internal/types/interfaces.go
+++ internal/types/interfaces.go
@@ -10,7 +10,12 @@ type Closer interface {
 	Close()
 }
 
-type StringWriter interface {
+// Represents a writer backed by an in-memory buffer
+type BufferBackedWriter interface {
 	io.Writer
 	io.StringWriter
+
+	// Bytes returns a slice holding the unread portion of the internal buffer.
+	// The slice aliases the buffer content at least until the next buffer modification.
+	Bytes() []byte
 }
